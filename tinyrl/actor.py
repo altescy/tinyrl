5a5ceafc38e7ce25466704c68b11d13a945a584c
altescy <altescy@fastmail.com>
2024-07-10 16:16:42 +0900

    modify methods

--- tinyrl/actor.py
+++ tinyrl/actor.py
@@ -6,5 +6,8 @@ _T_Action = TypeVar("_T_Action")
 
 
 class BaseActor(Generic[_T_Action]):
-    def __call__(self, probs: torch.Tensor) -> tuple[_T_Action, torch.Tensor]:
+    def select(self, probs: torch.Tensor, action: _T_Action) -> torch.Tensor:
+        raise NotImplementedError
+
+    def sample(self, probs: torch.Tensor) -> tuple[_T_Action, float]:
         raise NotImplementedError
